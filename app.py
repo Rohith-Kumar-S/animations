9939dfc1b4ba18f58c7b5faea69041ceabe74487
Rohith S <rohithkumarvsk@gmail.com>
2025-09-12 20:55:33 -0400

    init commit2

--- app.py
+++ app.py
@@ -2,21 +2,35 @@ import streamlit as st
 import cv2
 import numpy as np
 st.title("Live Webcam Feed")
+if "stop" not in st.session_state:
+    st.session_state["stop"] = False
+if "start" not in st.session_state:
+    st.session_state["start"] = True
 
 # Create a placeholder for the video frames
-# frame_window = st.empty()
-
+frame_window = st.empty()
+if st.button("Stop"):
+    st.session_state["stop"] = True
+if st.button("Start"):
+    st.session_state["start"] = True
+    st.session_state["stop"] = False
 # Open default webcam (use a file path or URL for other sources)
-enable = st.checkbox("Enable camera")
-picture = st.camera_input("Take a picture", disabled=not enable)
+if st.session_state["start"]:
+    cap = cv2.VideoCapture(0)
+    # Loop until user stops the app
+    while True:
+        if st.session_state["stop"]:
+            cap.release()
+            break
+        ret, frame  = cap.read()
+        if not ret:
+            st.write("Failed to grab frame")
+            break
 
-# Loop until user stops the app
-if enable and picture is not None:
-    bytes_data = picture.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+        # Convert BGR (OpenCV default) to RGB for correct colors
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    # Convert BGR (OpenCV default) to RGB for correct colors
-    frame = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
+        # Display the frame in the Streamlit app
+        frame_window.image(frame, channels="RGB")
+    
 
-    # Display the frame in the Streamlit app
-    st.image(frame, channels="RGB")
